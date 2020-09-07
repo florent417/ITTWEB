d05ab6310f05927f22037d4114a0641e93fb82ce
florent417 <36982701+florent417@users.noreply.github.com>
2020-09-07 22:08:37 +0200

    2a d, missing left menu and footer

--- L2_node_express_EJS_responsive/LAB_2A/pug_ex/routes/students.js
+++ L2_node_express_EJS_responsive/LAB_2A/pug_ex/routes/students.js
@@ -1,15 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+let Student = {
+  name: "Invertebrates",
+  grade: "Invertebrates"
+};
+
+const students = [];
+
+/* GET students listing. */
 router.get('/', function(req, res, next) {
   res.render('studentForm', { title: 'Black board' });
 });
 
 // POST student data
 router.post("/add_student", function(req, re, next){
-  console.log(req.body.name);
-  console.log(req.body.grade);
-})
+  students.push(createStudentObj(req));
+  console.log(students.length);
+});
+
+/* Get all students*/
+router.get('/all_students', function(req, res, next) {
+  res.render('all_students', { students });
+});
+
+let createStudentObj = req =>{
+  let student = Object.create(Student);
+  student.name = req.body.name;
+  student.grade = req.body.grade;
+  console.log("Created student. " + student);
+  return student;
+}
 
 module.exports = router;
